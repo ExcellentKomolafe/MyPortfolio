9c161dae4323cc5212da7dc0620d33c9b2927659
Excellent Komolafe <ayoolaexcellent2@gmail.com>
2025-11-03 08:11:31 +0100

    Add no-op build script for Render

--- Excellent-Portfolio/src/components/Body/ProjectCard.jsx
+++ Excellent-Portfolio/src/components/Body/ProjectCard.jsx
@@ -1,6 +1,6 @@
 function ProjectCard({imgURL, des, desTitle}) {
   return(
-    <div className="w-fit lg:w-1/2  mx-auto">
+    <div id="projects" className="w-fit lg:w-1/2  mx-auto">
         <img src={imgURL} alt="project image" className="w-9/10 mx-auto rounded-t-xl"/>
         <div className=" mx-auto bg-black rounded-b-2xl py-4 font-sans pl-2 w-9/10 text-white">
           <h2 className="text-2xl my-2">{desTitle}</h2>
